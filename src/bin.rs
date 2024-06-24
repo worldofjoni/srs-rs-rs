63bb04edc78137ce7e309871a3140010cede5931
Jonatan Ziegler <udslu@student.kit.edu>
2024-06-24 16:40:14 +0200

    cleanup

--- src/bin.rs
+++ src/bin.rs
@@ -4,5 +4,8 @@ use recsplit::{mvp::MVP, LooseRecSplit};
 fn main() {
     LooseRecSplit::new_random(&(0..100).collect::<Vec<_>>());
 
-    MVP::new_random(&(0..100).collect::<Vec<_>>().chunks(10).collect::<Vec<_>>());
+    MVP::new_random(
+        &(0..100).collect::<Vec<_>>().chunks(10).collect::<Vec<_>>(),
+        0.01,
+    );
 }
